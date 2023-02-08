6792eb1ba6430a2e6475365b194de16715b18f94
Spydr06 <spydr06@web.de>
2023-02-08 17:44:49 +0100

    Implement setjmp and longjmp

--- libc/include/iso646.h
+++ libc/include/iso646.h
@@ -3,4 +3,20 @@
 
 #include <sys/cdefs.h>
 
+#ifndef __cplusplus
+
+#define and    &&
+#define and_eq &=
+#define bitand &
+#define bitor  |
+#define compl  ~
+#define not    !
+#define not_eq !=
+#define or     ||
+#define or_eq  |=
+#define xor    ^
+#define xor_eq ^=
+
+#endif
+
 #endif /* _ISO_646_H */
--- libc/include/setjmp.h
+++ libc/include/setjmp.h
@@ -3,4 +3,27 @@
 
 #include <sys/cdefs.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef unsigned long __jmp_buf[6];
+
+typedef struct {
+    __jmp_buf __jb;
+    unsigned long __fl;
+    unsigned long __ss[128 / sizeof(long)];
+} jmp_buf[1];
+
+#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 1)
+#define __setjmp_attr __attribute__((__returns_twice__))
+#endif
+
+int setjmp(jmp_buf) __setjmp_attr;
+void longjmp(jmp_buf, int);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif /* _SETJMP_H */
